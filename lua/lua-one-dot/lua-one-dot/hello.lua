98581f99854c1aa039801f90215d7b691a26749c
dolphilia <42200736+dolphilia@users.noreply.github.com>
2022-08-01 20:17:29 +0900

    lua one-dot 最初のコミット

---
+++ lua/lua-one-dot/lua-one-dot/hello.lua
@@ -0,0 +1,5 @@
+﻿-- hello.lua
+-- print 関数は自動的に "\n" を入れることを忘れない
+print("Hello Lua World")
+print("你好 Lua")
+print("こんにちは Lua")
---
+++ lua/lua-one-dot/lua-one-dot/lua-one-dot.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+int main(int argc, char* argv[]) {
+	const char* fn = "hello.lua";
+	// Lua の言語エンジンを初期化
+	lua_State* lua = luaL_newstate();
+
+	// Lua のライブラリを使えるようにする
+	luaL_openlibs(lua);
+
+	// Lua のスクリプトを読み込む
+	printf("%d\n", luaL_dofile(lua, fn));
+
+	return 0;
+}
