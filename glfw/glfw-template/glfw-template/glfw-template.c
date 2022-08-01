cd248e96302e6234ea7a0047fc2b8955aca5fb4c
dolphilia <42200736+dolphilia@users.noreply.github.com>
2022-08-01 20:18:29 +0900

    one-dot のテンプレートプロジェクト

---
+++ glfw/glfw-template/glfw-template/glfw-template.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (glfwInit() != GL_TRUE) {
+        fprintf(stderr, "Failed to initialize GLFW.\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    window = glfwCreateWindow(640, 480, "Step 01", NULL, NULL);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create window.\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
