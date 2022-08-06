5d0b06351303e14107de1a29f2da1d3b3ec4e270
dolphilia <dolphilia@outlook.com>
2022-08-06 14:44:23 +0900

    for macos

--- glfw/glfw-template/glfw-template/glfw-template.c
+++ glfw/glfw-template/glfw-template/glfw-template.c
@@ -1,5 +1,14 @@
-﻿#include <stdio.h>
+﻿//
+// macos:
+// clang glfw-template.c -o glfw-template -lglfw -framework OpenGL -I /opt/homebrew/include -L /opt/homebrew/lib
+//
+#include <stdio.h>
 #include <stdlib.h>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#endif
+
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -11,11 +20,12 @@ int main(void)
         return EXIT_FAILURE;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    // この記述があるとmacosでウィンドウ生成に失敗する
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    //glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     window = glfwCreateWindow(640, 480, "Step 01", NULL, NULL);
 
