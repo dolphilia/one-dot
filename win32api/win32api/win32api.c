f3199d0902da5cf0569eb4d226eea6d5ad9af5fc
dolphilia <42200736+dolphilia@users.noreply.github.com>
2022-09-05 22:33:37 +0900

    win32api update

---
+++ win32api/win32api/win32api.c
@@ -0,0 +1,15 @@
+﻿//必要なヘッダーファイルのインクルード
+#define STRICT
+#include <windows.h>
+#include <tchar.h>
+
+//--------------------------------------------
+// Name:WinMain()
+// Desc:アプリケーションのエントリー関数
+//--------------------------------------------
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
+{
+    MessageBox(NULL, _T(""), _T(""), MB_OK);
+
+    return 0;
+}
